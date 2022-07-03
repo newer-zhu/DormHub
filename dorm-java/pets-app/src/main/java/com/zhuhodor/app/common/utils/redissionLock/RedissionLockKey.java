fb79bc241ecc6745117519de072c7a27e8731de6
zhuhodor <zhd30@outlook.com>
2022-07-03 23:06:16 +0800

    change modules

--- dorm-java/pets-app/src/main/java/com/zhuhodor/app/common/utils/redissionLock/RedissionLockKey.java
+++
@@ -1,9 +0,0 @@
-package com.zhuhodor.app.common.utils.redissionLock;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.PARAMETER, ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Inherited
-public @interface RedissionLockKey {
-}
--- dorm-java/pets-model/src/main/java/com/zhuhodor/app/model/vo/echarts/ColumnVo.java
+++
@@ -1,8 +0,0 @@
-package com.zhuhodor.app.model.vo.echarts;
-
-import lombok.Data;
-
-@Data
-public class ColumnVo {
-
-}
